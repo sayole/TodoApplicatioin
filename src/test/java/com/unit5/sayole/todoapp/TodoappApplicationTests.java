e5a696a6977050d166f1639ec0ec69ca53832953
sayole <sylee8191@gmail.com>
2023-02-14 22:59:47 +0900

    initial commit

---
+++ src/test/java/com/unit5/sayole/todoapp/TodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unit5.sayole.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
